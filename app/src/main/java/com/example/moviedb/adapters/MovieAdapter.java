7114ce209fe881c733d08490f043868873e7af95
ShweYamoneOO <shweyamoneoo1234@gmail.com>
2019-11-13 16:35:03 +0630

    symo commit

---
+++ app/src/main/java/com/example/moviedb/adapters/MovieAdapter.java
@@ -0,0 +1,77 @@
+package com.example.moviedb.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.moviedb.R;
+import com.example.moviedb.common.BaseAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.example.moviedb.util.AppConstant.BASE_IMG_URL;
+
+public class MovieAdapter extends BaseAdapter {
+    @Override
+    protected RecyclerView.ViewHolder onCreateCustomViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie,parent,false);
+
+        return new MovieAdapter.ViewHolder(view);
+    }
+
+    @Override
+    protected void onBindCustomViewHolder(RecyclerView.ViewHolder holder, int position) {
+    //    ((MovieAdapter.ViewHolder)holder).bindView((MovieInfoModel)getItemsList().get(position),position);
+    }
+
+    @Override
+    protected RecyclerView.ViewHolder onCreateCustomHeaderViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    protected void onBindCustomHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.iv_movie_poster)
+        ImageView ivMoviePoster;
+
+
+    //    MovieInfoModel mMovieModel;
+
+        private Context context;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            context = itemView.getContext();
+            ButterKnife.bind(this,itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                //    context.startActivity(MovieDetailActivity.getMainActivityIntent(context,mMovieModel.getId()));
+
+                }
+            });
+        }
+/*
+        public void bindView(MovieInfoModel model, int position) {
+
+            this.mMovieModel = model;
+
+            Glide.with(context)
+                    .load(BASE_IMG_URL+model.getPoster_path())
+                    .into(ivMoviePoster);
+
+        }*/
+    }
+}
