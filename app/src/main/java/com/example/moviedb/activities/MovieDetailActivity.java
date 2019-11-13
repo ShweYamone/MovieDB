4b21631dfee1b49773fce5102f0ddd015aaa9b03
nannsumonkyaw <nannsumonkyaw1998@gmail.com>
2019-11-13 15:05:41 +0630

    -nsmk commit

---
+++ app/src/main/java/com/example/moviedb/activities/MovieDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.moviedb.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.moviedb.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
