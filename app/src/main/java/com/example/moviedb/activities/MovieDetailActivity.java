1735f84758e1f14b46103b60ceb19e180ee8fdae
nannsumonkyaw <nannsumonkyaw1998@gmail.com>
2019-11-20 16:02:11 +0630

    -commit

--- app/src/main/java/com/example/moviedb/activities/MovieDetailActivity.java
+++ app/src/main/java/com/example/moviedb/activities/MovieDetailActivity.java
@@ -220,6 +220,7 @@ public class MovieDetailActivity extends BaseActivity implements MovieDetailView
 
         //if internet isn't availabel ,load data from db
         if (!mNetwork.isNetworkAvailable()) {
+
             dbHelper.movieDAO().getMovieInfobyId(mmovieId);
             movieTitle.setText(movie.getMovieName());
             releaseDate.setText(movie.getReleaseDate());
@@ -354,7 +355,7 @@ public class MovieDetailActivity extends BaseActivity implements MovieDetailView
 
                         }
 
-                    
+
 
                 }
 
